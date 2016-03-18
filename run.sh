58da00853bd7c7a1793eace3421fdd1e0b70b1ed
Elifcan Cakmak <elifcan88@gmail.com>
2016-03-18 13:41:13 +0200

    add run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
+if [ $# != 2 ]
+  then
+    echo "Usage: run.sh ROOM_ID AUTH_TOKEN"
+    exit 1;	
+fi
+
 outage=$(casperjs checkForOutage.js);
 
 if [[ $outage == *"found results 0"* ]]
 then
-	echo "kesinti yok";
+	exit;
 else
 	if [[ $outage == *"KARŞIYAKA"* ]]
 	then
-		echo "kesinti var";
+		ROOM_ID=$1
+		AUTH_TOKEN=$2
+		MESSAGE="kesinti var"
+
+		curl -H "Content-Type: application/json" \
+		     -X POST \
+		     -d "{\"color\": \"purple\", \"message_format\": \"text\", \"message\": \"$MESSAGE\" }" \
+		     https://api.hipchat.com/v2/room/$ROOM_ID/notification?auth_token=$AUTH_TOKEN
 	else
-		echo "kesinti yok";
+		exit;
 	fi
 fi
 
 
-
-
-#if result = 0 then exit olarak değiştirilecek.
